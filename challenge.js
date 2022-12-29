11f9b270b5d6050cf26f9cb574dc024728cdb945
Evans Rintari <rintari110@gmail.com>
2022-12-29 21:56:04 +0300

    first commit

---
+++ challenge.js
@@ -0,0 +1,50 @@
+/*const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};*/
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 545, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
+
+// OBJECT CHALLENGE
+const mark = {
+  fullName: " mark Smith",
+  mass: 78,
+  height: 1.69,
+
+  calcBMI: function () {
+    // console.log(this);
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Miller",
+  Weight: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.Weight / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI ${mark.bmi} is higher than ${john.fullName}'s ${john.bmi}`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI ${john.bmi} is higher than ${mark.fullName}'s ${mark.bmi}`
+  );
+}
---
+++ script.js
@@ -0,0 +1,216 @@
+"use strict";
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+
+// if (hasDriversLicense) console.log("i can drive");
+/*
+function logger() {
+  console.log("My name is Jonas");
+}
+
+// call the function, running, invocking
+
+logger();
+logger();
+
+//function can recieve data and return data as well
+// data is passed into a function
+// thinking a function as machine.
+
+// parameter are like variables,
+//  that are only specific to this function
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+
+  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+// argument
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+*/
+// different way of writting function
+// in function declaration you can call
+//  a function befor you declare it
+// happen because of hoisting
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// annymous function ---function expression
+// const calcAge2 = function (birthYear) {
+// return 2037 - birthYear;
+// };
+// const age2 = calcAge2(1971);
+// console.log(age2);
+//
+// Arrow function
+// shorter and faster to write-- return happen implicitly
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(2002);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement}`;
+};
+console.log(yearsUntilRetirement(1999, "jonas"));
+console.log(yearsUntilRetirement(2002, "krishna"));
+
+// calling one function inside another function
+function cutFruitpieces(fruits) {
+  return fruits * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitpieces(apples);
+  const orangePieces = cutFruitpieces(oranges);
+
+  const juice = `juice with ${applePieces} apples and ${orangePieces} oranges`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+// ARRAYS
+// arrays are zero based
+/*
+const friends = ["Micheal", "steven", "peter"]; //leteral syntax
+console.log(friends);
+const years = new Array(1991, 1992, 200, 2020);
+console.log(years);
+console.log(friends[0]);
+
+// getting the actual number of property that are in the array
+console.log(friends.length);
+
+//retrieving an element from an array
+
+console.log(friends[friends.length - 1]);
+
+// mutating the array or changing the array
+friends[2] = "jay";
+console.log(friends);
+
+const rintari = ["jonas", "evans", "rintari", 2037 - 1919, "learner", friends];
+console.log(rintari);
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const year = [1990, 1997, 2002, 2018];
+const ages = [
+  calcAge(year[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);*/
+
+// /methods ae Array function
+
+const friends = ["Micheal", "steven", "peter"];
+//since push is a function it can therefor return something
+//
+const newLength = friends.push("jay");
+friends.unshift("geradd");
+console.log(newLength);
+console.log(friends);
+
+// remove
+friends.pop();
+//return something i.e wat is being removed
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+// include it return true if the element is in the array
+
+// rthis metod can be used to write coditionals
+console.log(friends.includes("steven"));
+console.log(friends.includes("bob"));
+
+if (friends.includes("steven")) {
+  console.log("you have the friend called steven");
+}
+
+// OBJECTS
+// is a data structure we actually define key value pairs
+// we use curly bracess
+/*const jonas = {
+  firstName: "jonas",
+  lastName: "schedmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["martin", "leo", "syste"],
+};
+// the order does not matter at all when we want to retrieve them
+// how do weget data
+// how to dow retrieve the data uasing the dATA  notation and bracket
+
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas["firstName"]);
+
+/*const interestedIn = prompt(
+  "what do you want to know about jonas? Choose between job,firstname, lastname, friends, age"
+);
+//
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log(
+    'wrong request! Choose between job,firstname, lastname, friends, age" '
+  );
+}
+*/ /*
+// adding new location
+jonas.location = "portugal";
+jonas["twiter"] = "@jonasschemedman";
+console.log(jonas);
+//  challenge
+
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length}, and his bnest friend is called${jonas.friends[0]}`
+);*/
+
+const jonas = {
+  firstName: "jonas",
+  lastName: "schedmann",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["martin", "leo", "syste"],
+  hasDriversLicense: true,
+  // always keep the code dry
+  //  special variable called(this)
+  //this is == to the object  calling the method
+  //this point to jonas(object)
+
+  calcAge4: function () {
+    console.log(this);
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge4()} year old ${
+      this.job
+    }, and he has  ${this.hasDriversLicense ? "a" : "NO"} drivers lincences`;
+  },
+};
+console.log(jonas.calcAge4());
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+// challange 2
+console.log(jonas.getSummary());
