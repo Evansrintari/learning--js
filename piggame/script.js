4dc78f2fc86d806107f77e4a6d4e7721b36316fa
EvansRintari <rintari110@gmail.com>
2023-01-18 12:28:34 +0300

    message

--- piggame/script.js
+++ piggame/script.js
@@ -1,1 +1,48 @@
 'use strict';
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
+
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
+
+const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//start conditions
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add('hidden');
+
+const score = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+// rolling cdice
+btnRoll.addEventListener('click', function () {
+  // generate a radom dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // display dice
+  diceEL.classList.remove('hidden');
+  diceEL.src = `dice-${dice}.png`;
+  //check for roll1: if true,
+
+  if (dice !== 1) {
+    currentScore += dice;
+    // current0EL.textContent = currentScore;
+
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // switch to the next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player0EL.classList.toggle('player--active');
+    player1EL.classList.toggle('player--active');
+  }
+});
